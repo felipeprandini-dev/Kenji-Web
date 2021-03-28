062656b1f26fd6059fea17c3e1ffb0c548376ecd
felipeprandini-dev <77051604+felipeprandini-dev@users.noreply.github.com>
2021-03-28 17:10:17 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,26 +18,6 @@ $('#myCarousel').on('slide.bs.carousel', function (e) {
         }
     }
 }); 
-$('#myCarousel2').on('slide.bs.carousel', function (e) {
-
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 4;
-    var totalItems = 6;
-     
-    if (idx >= totalItems-(itemsPerSlide-1)) {
-        var it = itemsPerSlide - (totalItems - idx);
-        for (var i=0; i<it; i++) {
-            // append slides to end
-            if (e.direction=="left") { 
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');
-            }
-            else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
-            }
-        }
-    }
-}); 
 
 
 $('.carousel').on('touchstart', function(event){
