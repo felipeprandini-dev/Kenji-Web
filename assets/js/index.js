22c5b0f3f3c487ec77709518cf34ce380bcce391
felipeprandini-dev <77051604+felipeprandini-dev@users.noreply.github.com>
2021-03-28 10:15:51 -0300

    A

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
       $("#myCarousel").on("slide.bs.carousel", function(e) {
               var $e = $(e.relatedTarget);
               var idx = $e.index();
@@ -22,8 +22,30 @@ $(document).ready(function() {
               }
 
       });
-      
-});      
+
+});      */
+$('#myCarousel').on('slide.bs.carousel', function (e) {
+    /*
+        CC 2.0 License Iatek LLC 2018 - Attribution required
+    */
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 5;
+    var totalItems = $('.carousel-item').length;
+ 
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx); 
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
 $('.carousel').on('touchstart', function(event){
     const xClick = event.originalEvent.touches[0].pageX;
     $(this).one('touchmove', function(event){
