1eda18e79f8062bbe10832ccfb9e0a3c466ebf2c
felipeprandini-dev <77051604+felipeprandini-dev@users.noreply.github.com>
2021-03-28 16:34:46 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,7 @@ $('#myCarousel').on('slide.bs.carousel', function (e) {
     var idx = $e.index();
     var itemsPerSlide = 4;
     var totalItems = $('.carousel-item').length;
-    
+     
     if (idx >= totalItems-(itemsPerSlide-1)) {
         var it = itemsPerSlide - (totalItems - idx);
         for (var i=0; i<it; i++) {
