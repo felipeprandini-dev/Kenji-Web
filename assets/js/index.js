3599e5dce5628d3b2ca130b4393d28114a131b66
felipeprandini-dev <77051604+felipeprandini-dev@users.noreply.github.com>
2021-03-29 23:45:48 -0300

    a

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,13 @@
 
     // manual carousel controls
     $('.next').click(function(){ $("#postsCarousel").carousel('next');return false; });
-    $('.prev').click(function(){ $("postsCarousel").carousel('prev');return false; });
+    $('.prev').click(function(){ $("#postsCarousel").carousel('prev');return false; });
     
+    $('.next').click(function(){ $("#postsCarousel2").carousel('next');return false; });
+    $('.prev').click(function(){ $("#postsCarousel2").carousel('prev');return false; });
+    
+    $('.next').click(function(){ $("#postsCarousel3").carousel('next');return false; });
+    $('.prev').click(function(){ $("#postsCarousel3").carousel('prev');return false; });
+    
+
 })(jQuery);
