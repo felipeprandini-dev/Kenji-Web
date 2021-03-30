087e313e2c3a1c2609c3eca0c66731843952a047
felipeprandini-dev <77051604+felipeprandini-dev@users.noreply.github.com>
2021-03-29 23:51:34 -0300

    a

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,14 +2,14 @@
     "use strict";
 
     // manual carousel controls
-    $('.next').click(function(){ $("#postsCarousel").carousel('next');return false; });
-    $('.prev').click(function(){ $("#postsCarousel").carousel('prev');return false; });
+    $("next1").click(function(){ $("#postsCarousel").carousel('next');return false; });
+    $(".prev1").click(function(){ $("#postsCarousel").carousel('prev');return false; });
     
-    $('.next').click(function(){ $("#postsCarousel2").carousel('next');return false; });
-    $('.prev').click(function(){ $("#postsCarousel2").carousel('prev');return false; });
+    $(".next2").click(function(){ $("#postsCarousel2").carousel('next');return false; });
+    $(".prev2").click(function(){ $("#postsCarousel2").carousel('prev');return false; });
     
-    $('.next').click(function(){ $("#postsCarousel3").carousel('next');return false; });
-    $('.prev').click(function(){ $("#postsCarousel3").carousel('prev');return false; });
+    $(".next3").click(function(){ $("#postsCarousel3").carousel('next');return false; });
+    $(".prev3").click(function(){ $("#postsCarousel3").carousel('prev');return false; });
     
 
 })(jQuery);
