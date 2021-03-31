94fbe3936fdced9c98249038990076bed5875342
felipeprandini-dev <77051604+felipeprandini-dev@users.noreply.github.com>
2021-03-31 00:08:53 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,31 @@
-$('.carousel').on('touchstart', function(event){
+$("#carousel").on('touchstart', function(event){
     const xClick = event.originalEvent.touches[0].pageX;
     $(this).one('touchmove', function(event){
         const xMove = event.originalEvent.touches[0].pageX;
         const sensitivityInPx = 5;
 
         if( Math.floor(xClick - xMove) > sensitivityInPx ){
-            $(this).carousel('next');
+            $("#carousel").carousel('next');
         }
         else if( Math.floor(xClick - xMove) < -sensitivityInPx ){
-            $(this).carousel('prev');
+            $("#carousel").carousel('prev');
+        }
+    });
+    $(this).on('touchend', function(){
+        $(this).off('touchmove'); 
+    });
+});
+$("#carousel2").on('touchstart', function(event){
+    const xClick = event.originalEvent.touches[0].pageX;
+    $(this).one('touchmove', function(event){
+        const xMove = event.originalEvent.touches[0].pageX;
+        const sensitivityInPx = 5;
+
+        if( Math.floor(xClick - xMove) > sensitivityInPx ){
+            $("#carousel2").carousel('next');
+        }
+        else if( Math.floor(xClick - xMove) < -sensitivityInPx ){
+            $("#carousel2").carousel('prev');
         }
     });
     $(this).on('touchend', function(){
